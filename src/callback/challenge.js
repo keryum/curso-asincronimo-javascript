4f2e5048b579aaf33ee9d0066abd6519bd46fb1d
keryum <alejandro_suarezp@outlook.com>
2023-05-10 21:05:34 -0500

    avance hasta uso de callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -6,15 +6,13 @@ function fetchData(urlApi, callback) {
 
     xhttp.open('GET', urlApi, true);
     xhttp.onreadystatechange = function(event) {
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error' + urlApi);
                 return callback(error, null);
             }
         }
-    }
     xhttp.send();
 }
 
@@ -23,7 +21,7 @@ fetchData(`${API}/products`, function(error1, data1) {
     fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
         if(error2) return console.error(error2);
         fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
-            if (error3) return console.log(error3);
+            if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
             console.log(data3.name);
